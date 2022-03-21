29ab0c8a7ee623a6d76db440c320a35c763c3d4e
Roikjer <asge1913@videndjurs.net>
2022-03-21 12:40:20 +0100

    "Funktionel" sidescroller

--- Assets/EnemyCollision.cs
+++ Assets/EnemyCollision.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
 {
+
+    public float speed;
+    public float jump;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
+
+        rb.velocity = new Vector2(speed, rb.velocity.y);
         
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.velocity = new Vector2(speed, jump);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)

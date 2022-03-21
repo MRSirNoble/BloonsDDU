306547846a6acd62025b1deda5dbdaab38309016
MRSirNoble <sercanmartinishoy@gmail.com>
2022-03-21 12:55:41 +0100

    Currency float

---
+++ Assets/Scripts/Currencyfloat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName ="ScriptableObjects/Currencyfloat")]
+public class Currencyfloat : ScriptableObject
+{
+
+    public float Currency;
+
+}

77cab37eed24d2550faae12c705e504caaafa33e
MRSirNoble <sercanmartinishoy@gmail.com>
2022-04-22 12:37:20 +0200

    Scriptable upgrade

---
+++ Assets/Scripts/EnableUpgrades.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnableUpgrades")]
+public class EnableUpgrades : ScriptableObject
+{
+    public bool EnableValue;
+
+
+}
